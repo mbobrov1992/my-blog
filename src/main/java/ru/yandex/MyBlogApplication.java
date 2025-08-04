84e11dcceab06211c1cb41c552b1f48543ad12fb
mobobrov <mobobrov@sberbank.ru>
2025-08-04 18:44:28 +0300

    init

---
+++ src/main/java/ru/yandex/MyBlogApplication.java
@@ -0,0 +1,10 @@
+package ru.yandex;
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+public class MyBlogApplication {
+
+  public static void main(String[] args) {
+  }
+}
